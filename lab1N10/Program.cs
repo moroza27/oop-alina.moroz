9ac861a37d9fa86f8bae8cd805426d2d0cb5cc92
Alia1327 <alinamoroz139@gmail.com>
2025-09-08 21:42:43 +0300

    Lab1: Added Computer class

---
+++ lab1N10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Computer
+{
+  private string cpu;
+  private int ram;
+
+  public int Storage { get; set; }
+
+  public Computer(string cpu, int ram, int storage)
+  {
+    this.cpu = cpu;
+    this.ram = ram;
+    Storage = storage;
+    Console.WriteLine("Computer created.");
+  }
+
+  ~Computer()
+  {
+    Console.WriteLine("Computer destroyed.");
+  }
+
+  public void RunBenchmark()
+  {
+    Console.WriteLine($"Running benchmark on {cpu} with {ram} GB RAM and {Storage} GB storage...");
+    Console.WriteLine("Benchmark completed!");
+  }
+}
